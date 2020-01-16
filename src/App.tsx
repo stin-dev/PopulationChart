983006123016c3f6ff7651f1b3ba28925dfdc1ea
Yosuke Hiraoka <yoh.s.c.sep10@gmail.com>
2020-01-16 15:35:17 +0900

    show result to get api.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PrefCheckBox } from './components/PrefCheckBox';
+import useAppState from "./AppState";
 
 const App: React.FC = () => {
+  const { checkedPrefs, checkboxClickHandler, populations, apiError } = useAppState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AppContainer">
+        <header>
+          <h1>Population Chart</h1>
+        </header>
+        <div className="PrefCheckBoxContainer">
+          {checkedPrefs.map(checkedPref => (
+            <PrefCheckBox
+              key={checkedPref.prefecture.prefCode}
+              checked={checkedPref.checked}
+              prefName={checkedPref.prefecture.prefName}
+              onClick={checkboxClickHandler(checkedPref.prefecture.prefCode)}
+            />
+          ))}
+        </div>
+        <div>
+          {populations.map(population => (
+            <React.Fragment key={population.prefecture.prefCode}>
+              <p>{`${population.prefecture.prefCode}: ${population.prefecture.prefName}`}</p>
+              <p>{JSON.stringify(population.population.data[0].data[0])}</p>
+            </React.Fragment>
+          ))}
+        </div>
+        {apiError && <p className="ErrorMessage">{apiError.message}</p>}
+      </div>
     </div>
   );
 }
