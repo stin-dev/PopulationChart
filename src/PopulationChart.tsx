b3c6e019709b8e83f2bdc17f22c1fbb6a0bf25ec
Yosuke Hiraoka <yoh.s.c.sep10@gmail.com>
2020-01-20 01:58:35 +0900

    receive chart data prop.

--- src/PopulationChart.tsx
+++ src/PopulationChart.tsx
@@ -3,48 +3,31 @@ import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
 } from 'recharts';
 import { randomColor } from './randomColor';
+import { ChartData } from './entites';
 
-const data = [
-  {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-];
-
-export const PopulationChart: React.FC = () => {
+export const PopulationChart: React.FC<{ data: ChartData[] }> = (props) => {
   return (
-    <ResponsiveContainer width="100%" aspect={1.6}>
+    <ResponsiveContainer width="95%" aspect={1.6}>
       <LineChart
-        data={data}
+        data={props.data}
         margin={{
           top: 5, right: 30, left: 20, bottom: 20,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="year" />
         <YAxis yAxisId="left" />
-        <YAxis yAxisId="right" orientation="right" />
         <Tooltip />
-        <Legend />
-        <Line yAxisId="left" type="monotone" dataKey="pv" stroke={randomColor()} activeDot={{ r: 8 }} />
-        <Line yAxisId="left" type="monotone" dataKey="uv" stroke={randomColor()} />
+        <Legend wrapperStyle={{ fontSize: "calc(6px + 2vmin)" }} />
+        {props.data[0] && Object.keys(props.data[0]).map(d => (
+          (d !== "year") &&
+          <Line key={d}
+            yAxisId="left"
+            type="monotone"
+            dataKey={d}
+            stroke={randomColor()}
+          />
+        ))}
       </LineChart>
     </ResponsiveContainer>
   );
